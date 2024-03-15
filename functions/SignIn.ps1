e83e0802f036acd9756c893bf2f93cb219bb342f
Marco Kleinert <marco.kleinert@gmail.com>
2024-03-15 15:14:53 +0100

    new: helper function to test signin

---
+++ functions/SignIn.ps1
@@ -0,0 +1,22 @@
+Function SignIn {
+    param ([Parameter(Mandatory)]$Connection)
+
+    try { 
+        $ctx = Get-AzContext
+    }
+    catch { 
+        Write-PSFMessage -Level Error -Message 'cannot create context'
+    }
+
+    if ($ctx) {
+        Write-PSFMessage -Level Verbose -Message 'Already signed in to Azure.'
+        $true
+    }
+    else {
+        Write-PSFMessage -Level Verbose -Message 'Not signed in to Azure.'
+        $false
+    }
+
+    
+
+}
